59f5591c05be942a0a3788625d726bb9ad3b95ec
Piotr Mińkowski <piotr_minkowski@wp.pl>
2020-04-15 00:04:59 +0200

    Adding Spring Cloud Netflix Eureka

---
+++ config-and-discovery/discovery-server/src/main/kotlin/pl/piomin/samples/discovery/DiscoveryServerApplication.kt
@@ -0,0 +1,13 @@
+package pl.piomin.samples.discovery
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer
+
+@SpringBootApplication
+@EnableEurekaServer
+class ConfigServerApplication
+
+fun main(args: Array<String>) {
+    runApplication<ConfigServerApplication>(*args)
+}

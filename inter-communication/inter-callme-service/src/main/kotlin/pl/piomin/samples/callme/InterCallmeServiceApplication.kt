ea02823325f9c221ddaceffce2d00ab974b481f7
piomin <piotr.minkowski@gmail.com>
2022-10-29 01:03:56 +0200

    update `callme-service`

--- inter-communication/inter-callme-service/src/main/kotlin/pl/piomin/samples/callme/InterCallmeServiceApplication.kt
+++ inter-communication/inter-callme-service/src/main/kotlin/pl/piomin/samples/callme/InterCallmeServiceApplication.kt
@@ -4,7 +4,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-open class CallmeServiceApplication
+class CallmeServiceApplication
 
 fun main(args: Array<String>) {
     runApplication<CallmeServiceApplication>(*args)
